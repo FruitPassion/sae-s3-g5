5fa4529d786180b45daa464964df54ab6bff4e49
FruitPassion <gamehotdl@gmail.com>
2023-10-20 14:51:14 +0200

    Modification auth.py
    Ajout des bases des routes dans auth.py + ajout des methodes (GET, POST)

--- controller/auth.py
+++ controller/auth.py
@@ -5,13 +5,36 @@ from model.personnel import getAllPersonnel
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/hello-world')
-def hello_world():
+@auth.route('/', methods=['GET', 'POST'])
+def choix_connexion():
     return "Hello World"
 
 
-@auth.route('/')
-def index():
-    personnel = getAllPersonnel()
-    return render_template('auth/index.html', personnel=personnel)
+@auth.route('/choix-formation', methods=['GET', 'POST'])
+def choix_formation_apprentis():
+    return "Hello world"
 
+
+@auth.route('/choix-eleve', methods=['GET', 'POST'])
+def choix_eleve_apprentis():
+    return "Hello world"
+
+
+@auth.route('/choix-eleve', methods=['GET', 'POST'])
+def choix_eleve_apprentis():
+    return "Hello world"
+
+
+@auth.route('/connexion-apprentis', methods=['GET', 'POST'])
+def connexion_apprentis():
+    return "Hello World"
+
+
+@auth.route('/connexion-personnel', methods=['GET', 'POST'])
+def connexion_personnel():
+    return "Hello World"
+
+
+@auth.route('/logout', methods=['GET', 'POST'])
+def logout():
+    return "Hello World"
