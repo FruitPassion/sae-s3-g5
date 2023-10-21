d4b666a5cd5dc83da17a7233be7cb425263af2e5
FruitPassion <gamehotdl@gmail.com>
2023-10-21 11:13:14 +0200

    correction erreur blueprint API

--- controller/api.py
+++ controller/api.py
@@ -3,7 +3,7 @@ import urllib.parse
 from flask import Blueprint, jsonify
 from custom_paquets.security import passwordStrenght
 
-api = Blueprint('api', __name__, url_prefix="api")
+api = Blueprint('api', __name__, url_prefix="/api")
 
 
 @api.route("/password-strengh/<password>", methods=["GET"])
