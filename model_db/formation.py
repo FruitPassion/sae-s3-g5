b5c555e7804e027fdc435d83941f1fb12283c159
FruitPassion <gamehotdl@gmail.com>
2023-10-23 10:02:26 +0200

    correction model Formation

--- model_db/formation.py
+++ model_db/formation.py
@@ -5,7 +5,7 @@ class Formation(db.Model):
     __tablename__ = 'Formation'
     __table_args__ = {'schema': 'db_fiches_dev'}
 
-    id_formation = db.Columndb.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_formation = db.Column(db.Integer, primary_key=True, autoincrement=True)
     intitule = db.Column(db.String(100), nullable=False)
     niveau_qualif = db.Column(db.String(25))
     groupe = db.Column(db.Integer)
