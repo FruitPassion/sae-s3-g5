099cbce4538f4f062ff20b365a9d784b06e1b8f2
FruitPassion <gamehotdl@gmail.com>
2023-10-27 14:35:42 +0200

    Ajout decorateurs connexion et connexion eleve

--- custom_paquets/decorateur.py
+++ custom_paquets/decorateur.py
@@ -5,12 +5,29 @@ from flask import session, redirect, url_for, abort
 from model.personnel import checkSuperAdmin
 
 
+def login_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not session.get("name"):
+            redirect(url_for("auth.choix_connexion"))
+        return func(*args, **kwargs)
+
+
 def admin_login_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if not session.get("name") or not checkSuperAdmin(session.get("name")):
-            redirect(url_for('auth.choix_connexion'))
+            redirect(url_for("auth.choix_connexion"))
         return func(*args, **kwargs)
+
     return decorated_function
 
 
+def apprenti_login_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not session.get("name") or session.get("name") != "apprenti":
+            redirect(url_for("auth.choix_connexion"))
+        return func(*args, **kwargs)
+
+    return decorated_function
