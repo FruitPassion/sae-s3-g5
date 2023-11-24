0531375861c735e9333bcc8bac9e4cb2b0121de0
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-11-24 16:47:12 +0100

    modifs interface educadmin

--- controller/educateur_admin.py
+++ controller/educateur_admin.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 
 from custom_paquets.decorateur import educadmin_login_required
+from model.apprenti import get_apprenti_by_login
+from model.ficheintervention import get_fiches_techniques_finies_par_login
 
 educ_admin = Blueprint("educ_admin", __name__, url_prefix="/educ-admin")
 
@@ -19,4 +21,13 @@ def affiche_choix(apprenti):
 @educ_admin.route("/<apprenti>/fiches", methods=["GET"])
 @educadmin_login_required
 def fiches_apprenti(apprenti):
-    return render_template("personnel/choix_fiches_apprenti.html", apprenti=apprenti)
+    """
+    Récupère toutes les fiches techniques de l'élève sélectionné et les affiche.
+
+    Permet de consulter les commentaires laissés par les éducateurs et l'élève en question. 
+
+    :return: rendu de la page fiches_techniques.html
+    """
+    apprenti_infos = get_apprenti_by_login(apprenti)
+    fiches = get_fiches_techniques_finies_par_login(apprenti)
+    return render_template("personnel/choix_fiches_apprenti.html", apprenti=apprenti_infos, fiches=fiches)
