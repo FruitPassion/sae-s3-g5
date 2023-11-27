4b80d14648f8f28821a4df6eebbcfd1bac9f129d
lilouuuuuuu <lilou.pedro@etu.iut-tlse3.fr>
2023-11-27 09:07:00 +0100

    test visualiser commentaires cip

--- tests/controller/test_cip.py
+++ tests/controller/test_cip.py
@@ -6,24 +6,25 @@ from custom_paquets.tester_usages import connexion_personnel
 Test des controller du fichier admin.py
 '''
 
-
 # Test de la route de redirection d'affichage des commentaires
-def test_redirection_connexion(client):
-    # Test connexion superadministrateur
+def test_redirection_commentaires(client):
+    # Test connexion CIP
     username = "FAR16"
     passw = "cip"
     connexion_personnel(client, username, passw)
+
     # identifiant de la fiche 1 d'apprenti
     id_fiche = 1
+
     # login d'un apprenti pour le test
     apprenti = "DAJ12"
 
     # A MODIFIER (Miri)
-    response = client.get(url_for("cip.visualiser_commentaires", apprenti = apprenti, idFiche = id_fiche))
+    response = client.get(url_for("cip.visualiser_commentaires", apprenti = apprenti, fiche = id_fiche))
 
     # Test d'accès à la route
     assert response.status_code == 200
 
     # Test de vérification de la route
     # A MODIFIER (Miri)
-    assert response.request.path == "/cip/<apprenti>/<idFiche>/visualiser-commentaires"
+    assert response.request.path == "/cip/"+apprenti+"/"+str(id_fiche)+"/commentaires"
