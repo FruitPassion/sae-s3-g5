e32fb8e72f20d7ec6098b7c174a6d54d99d838b9
lilouuuuuuu <lilou.pedro@etu.iut-tlse3.fr>
2023-12-04 14:31:44 +0100

    Tests routes des controllers educ simple et personnel

---
+++ tests/controller/test_educ_simple.py
@@ -0,0 +1,76 @@
+from flask import url_for
+from custom_paquets.tester_usages import connexion_personnel
+
+'''
+Test des controller du fichier educateur_simple.py
+'''
+
+username = "MAC10"
+passw = "101010"
+formation = "Parcours plomberie"
+apprenti = "ANG12"
+fiche = 8
+
+# Test de la route de redirection de connexion de l'éducateur simple
+def test_redirection_connexion(client):
+    connexion_personnel(client, username, passw)
+
+    response = client.get(url_for("personnel.choix_formation"))
+    
+    # Test d'accès à la route
+    assert response.status_code == 200
+
+    # Test de vérification de la route
+    assert response.request.path == "/personnel/choix-formation-personnel"
+
+
+# Test de la route choix formation (affichage liste apprentis de cette formation)
+def test_affichage_apprentis_formation(client):
+    # Test connexion educateur simple
+    connexion_personnel(client, username, passw)
+    response = client.get(url_for("personnel.choix_eleve", nom_formation = formation))
+
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == f"/personnel/choix-eleves/{formation}"
+
+
+# Test de la route choix apprenti (affichage des fiches techniques de l'apprenti sélectionné)
+def test_choix_apprenti(client):
+    # Test connexion éducateur simple
+    connexion_personnel(client, username, passw)
+    response = client.get(url_for("educ_simple.fiches_apprenti", apprenti = apprenti))
+
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == f"/educ-simple/{apprenti}/fiches"
+
+
+# Test de la route commentaires (affichage des commentaires d'une fiche technique de l'apprenti sélectionné)
+def test_lecture_commentaires(client):
+    # Test connexion éducateur simple
+    connexion_personnel(client, username, passw)
+    response = client.get(url_for("educ_simple.visualiser_commentaires", apprenti = apprenti, fiche = fiche))
+
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == f"/educ-simple/{apprenti}/{fiche}/commentaires"
+
+
+# Test de la route modification commentaire
+def test_modification_commentaire(client):
+    # Test connexion éducateur simple
+    connexion_personnel(client, username, passw)
+    response = client.get(url_for("educ_simple.modifier_commentaires", apprenti = apprenti, fiche = fiche))
+
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == f"/educ-simple/{apprenti}/{fiche}/modifier-commentaires"
--- tests/controller/test_personnel.py
+++ tests/controller/test_personnel.py
@@ -8,8 +8,8 @@ Test des controller du fichier personnel.py
 
 # identifiants de connexion pour les tests
 username = "ALL11"
-passw = "educadmin"
-# passw = "111111" ?
+# passw = "educadmin"
+passw = "111111" 
 nom_formation = "Parcours maintenance batiment"
 apprenti = "ANG12"
 
@@ -21,7 +21,7 @@ def test_redirection_connexion(client):
     response = client.get(url_for("personnel.choix_formation"))
 
     # Test d'accès à la route
-    assert response.status_code == 200
+    assert response.status_code == 302
 
     # Test de vérification de la route
     assert response.request.path == "/personnel/choix-formation-personnel"
@@ -33,7 +33,7 @@ def test_choix_eleve(client):
     response = client.get(url_for("personnel.choix_eleve", nom_formation=nom_formation))
 
     # Test d'accès à la route
-    assert response.status_code == 200
+    assert response.status_code == 302
 
     # Test de vérification de la route
     assert response.request.path == f"/personnel/choix-eleves/{nom_formation}"
