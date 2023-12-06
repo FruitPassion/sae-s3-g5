edbe70c6f316895598f110cc87a8721d37f874ed
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-06 09:46:24 +0100

    création graphique suivi progression cip

--- static/js/suivi_progression_cip.js
+++ static/js/suivi_progression_cip.js
@@ -1,19 +1,35 @@
-import { Chart } from 'chart.js';
+function creation_graphique_suivi_progression_cip() {
+  var chart_suivi_progression = document.getElementById('suivi_progression_cip');
+  var context = chart_suivi_progression.getContext('2d');
 
-var doc = document.getElementById('suivi_progression_cip');
+  var bar_chart = new Chart(context, {
+    type: 'bar',
+    data: {
+      labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"],
+      datasets: [{
+        label: 'Nombre de fiches créées',
+        // jeu de données random
+        data: [3, 10, 5, 2, 7],
+        backgroundColor: ["#ffcd56", "#ff6384", "#36a2eb", "#fd6b19", "#4bc0c0"],
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Nombre de fiches créées par jour'
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            size: 25
+          }
+        }]
+      }
+    }
+  });
+}
 
-const data = {
-  datasets: [{
-    label: 'Suivi de progression',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
-  }]
-};
-
-const config = {
-  type: 'line',
-  data: data,
-};
-
-var myChart = new Chart(doc, config);
+window.onload = function() {
+  creation_graphique_suivi_progression_cip();
+}
