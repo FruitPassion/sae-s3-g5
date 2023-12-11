818ae6b825953d2dfaf87631f6737dd5837dbed8
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-11 11:43:48 +0100

    suivi progression apprenti graphique

--- static/js/suivi_progression_apprenti.js
+++ static/js/suivi_progression_apprenti.js
@@ -1,30 +1,32 @@
-function creation_graphique_suivi_progression_cip(etat_fiches) {
+function creation_graphique_suivi_progression_apprenti(etat_fiches) {
     var chart_suivi_progression = document.getElementById('camembert');
     var context = chart_suivi_progression.getContext('2d');
-    const fiches_terminees = 0;
-    const fiches_arretees = 0;
-    const fiches_en_cours = 0;
-    for (const etat of etat_fiches) {
-        if (etat === "1") {
+    var fiches_terminees = 0;
+    var fiches_arretees = 0;
+    var fiches_en_cours = 0;
+    var id = Object.keys(etat_fiches);
+    for (let i=0; i<id.length; i++) {
+        let etat = etat_fiches[id[i]];
+        if (etat === 1) {
             fiches_terminees++;
-        } else if (etat === "2") {
+        } else if (etat === 0) {
             fiches_en_cours++;
         } else {
             fiches_arretees++;
         }
     }
-
+    
     var chart = new Chart(context, {
-        type: 'doughnut',
+        type: 'pie',
         data: {
             labels: ['Fiches terminées', 'Fiches en cours', 'Fiches arrêtées'],
             datasets: [{
                 label: 'Fiches',
                 data: [fiches_terminees, fiches_en_cours, fiches_arretees],
                 backgroundColor: [
-                    'rgb(0, 255, 0)',
-                    'rgb(255, 255, 0)',
-                    'rgb(255, 0, 0)'
+                    '#98FB98',
+                    '#FF8B28',
+                    '#C30010'
                 ],
                 hoverOffset: 4
             }]
@@ -32,14 +34,28 @@ function creation_graphique_suivi_progression_cip(etat_fiches) {
         options: {
             plugins: {
                 legend: {
-                    position: 'bottom',
+                    position: 'right',
                     labels: {
                         font: {
                             fontFamily: 'Montserrat',
-                            size: 20
+                            size: 30
                         }
                     }
-                }
-            }
+                },
+                datalabels: {
+                    color: 'black',
+                    anchor: 'center',
+                    font: {
+                        size: 30,
+                        weight: 'bold'
+                    },
+                    formatter: (value, context) => {
+                        return value;
+                    }
+                },
+            },
+            responsive: true,
+            maintainAspectRatio: true,
         }
     });
+}
