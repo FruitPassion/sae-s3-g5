d5bcaf31a320482f753ba587d3795138c135c859
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-13 12:19:44 +0100

    test ajout personnel (en cours)

--- tests/controller/test_admin.py
+++ tests/controller/test_admin.py
@@ -1,5 +1,8 @@
 from flask import url_for
-
+import unidecode
+from custom_paquets.custom_form import AjouterPersonnel
+from model_db.personnel import Personnel
+from model_db.shared_model import db
 from custom_paquets.tester_usages import connexion_personnel
 
 '''
@@ -53,5 +56,28 @@ def test_redirection_gestion_personnel(client):
     assert response.status_code == 302
 
     # Test de vérification de la route
-    assert response.request.path == "/admin/gestion-personnel"
+    assert response.request.path == "/admin/gestion-personnel" 
 
+"""
+JSP COMMENT FAIRE HELP
+# Test de l'ajout d'un personnel
+def test_ajouter_personnel(client):
+    try:
+        form = AjouterPersonnel()
+        data = dict(
+            role="Educateur",
+            nom="Supreme",
+            prenom="Leader",
+            email="mail@mail.com",
+            password="000000",
+            login=unidecode(form.nom.data[0:2].upper().strip()) + unidecode(form.prenom.data[0].upper().strip()) + str(
+                len(form.nom.data.strip() + form.prenom.data.strip())).zfill(2)
+        )
+        response = client.post(url_for("admin.gestion_personnel"), data=data)
+    finally:
+        response = client.get(url_for("admin.gestion_personnel"))
+        assert response.status_code == 302
+        assert response.request.path == "/admin/gestion-personnel"
+        assert db.session.query(Personnel).filter(Personnel.login == "LES13").first() is not None
+        db.rollback()
+"""
