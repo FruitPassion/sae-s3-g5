21bd7047d9eac4bb143251e657798d373e4088c5
prescilla-estrade <prescilla.estrade@etu.iut-tlse3.fr>
2023-12-13 11:44:32 +0100

    modif gestion formations

--- static/js/gestion_formations.js
+++ static/js/gestion_formations.js
@@ -1,7 +1,18 @@
 function confirmationSuppression(element, intitule, id_formation) {
-    if (confirm('Voulez-vous vraiment supprimer la formation : ' + intitule + " ?")) {
-        supprimerFormation(id_formation);
-    }
+    const formationCells = document.querySelectorAll(`#pers-${id_formation} td`);
+    formationCells.forEach(cell => {
+        cell.style.backgroundColor = '#F8D7DA';
+    });
+    
+    setTimeout(() => {
+        if (confirm('Voulez-vous vraiment supprimer la formation : ' + intitule + " ?")) {
+            supprimerFormation(id_formation);
+        } else {
+            formationCells.forEach(cell => {
+                cell.style.backgroundColor = '';
+            });
+        }
+    }, 100);
 }
 
 function supprimerFormation(id_formation) {
