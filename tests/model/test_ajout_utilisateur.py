3c5d1ef22e7474429943f03a6df88fd35af2c79f
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-15 15:35:08 +0100

    test ajout utilisateur (à améliorer éventuellement)

--- tests/model/test_ajout_utilisateur.py
+++ tests/model/test_ajout_utilisateur.py
@@ -1,9 +1,10 @@
 from unidecode import unidecode
+from model_db.apprenti import Apprenti
 from model_db.personnel import Personnel
 from model_db.shared_model import db
 
 # Test de l'ajout d'un personnel
-def test_ajouter_personnel():
+def test_ajouter_personnel(client):
     role="Educateur"
     nom="Supreme"
     prenom="Leader"
@@ -20,4 +21,18 @@ def test_ajouter_personnel():
     db.session.rollback()
     assert db.session.query(Personnel).filter(Personnel.login == "LES13").first() is None
     
-# Test de l'ajout d'un apprenti (mais la fonction est dans le model apprenti ??)
+#Test de l'ajout d'un apprenti
+def test_ajouter_apprenti(client):
+    nom="SousFifre"
+    prenom="Malheureux"
+    photo="/url/photo.jpg"
+    login=unidecode(prenom[0:2].upper().strip()) + unidecode(nom[0].upper().strip()) + str(
+            len(nom.strip() + prenom.strip())).zfill(2)
+    
+    ## Copie du code dans add_apprenti pour tester la fonction et éviter les commit() dans les tests
+    apprenti = Apprenti(nom=nom, prenom=prenom, login=login, photo=photo)
+    db.session.add(apprenti)
+    
+    assert db.session.query(Apprenti).filter(Apprenti.login == "MAS19").first() is not None
+    db.session.rollback()
+    assert db.session.query(Apprenti).filter(Apprenti.login == "MAS19").first() is None
