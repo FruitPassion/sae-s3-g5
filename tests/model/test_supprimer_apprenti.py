714305d455daf8992396b905abb4afb9c93ee358
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-18 10:32:06 +0100

    modif test suppr apprenti

--- tests/model/test_supprimer_apprenti.py
+++ tests/model/test_supprimer_apprenti.py
@@ -10,8 +10,9 @@ login_apprenti=generate_login(nom_apprenti, prenom_apprenti)
 apprenti = Apprenti(nom=nom_apprenti, prenom=prenom_apprenti, login=login_apprenti, photo=photo)
 
 def test_supprimer_apprenti(client):
-    id_apprenti = add_apprenti(login_apprenti, nom_apprenti, prenom_apprenti, photo, commit=True)
+    id_apprenti = add_apprenti(login_apprenti, nom_apprenti, prenom_apprenti, photo, commit=False)
     
-    remove_apprenti(id_apprenti, commit=True)
+    remove_apprenti(id_apprenti, commit=False)
     assert db.session.query(Apprenti).filter(Apprenti.id_apprenti == id_apprenti).first() == None
     assert FicheIntervention.query.filter_by(id_apprenti=id_apprenti).all() == []
+    db.session.rollback()
