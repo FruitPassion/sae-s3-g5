aa12b40a5f957d4a1b3dfc9d68a0e0280aea9d76
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2023-12-18 17:31:41 +0100

    test_api fini (pour les fonctions qui sont dedans)

--- tests/controller/test_api.py
+++ tests/controller/test_api.py
@@ -1,8 +1,10 @@
 from flask import url_for
+from custom_paquets.converter import generate_login
 from custom_paquets.tester_usages import connexion_personnel_mdp
 from model.formation import add_formation, get_formation_id
+from model.personnel import get_id_personnel_by_login
 from model_db.shared_model import db, Apprenti
-from model.apprenti import update_nbr_essaies_connexion
+from model.apprenti import add_apprenti, get_apprenti_by_login, get_id_apprenti_by_login, update_nbr_essaies_connexion
 
 '''
 Test des controller du fichier api.py
@@ -69,3 +71,115 @@ def test_api_archiver_formation(client):
     # Test de vérification de l'archivage
     assert response.json["valide"] == True
     
+    # Test de désarchivage
+    response = client.get(url_for("api.api_desarchiver_formation", id_formation=id_formation))
+    
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == "/api/desarchiver-formation/"+str(id_formation)
+    
+    # Test de vérification du désarchivage
+    assert response.json["valide"] == True
+    
+    # Test de suppression
+    response = client.get(url_for("api.api_supprimer_formation", id_formation=id_formation))
+    
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == "/api/supprimer-formation/"+str(id_formation)
+    
+    # Test de vérification de la suppression
+    assert response.json["valide"] == True
+    
+# Test de l'archivage d'un apprenti
+def test_archivage_apprenti(client):
+    # Partie archivage
+    # Création d'un apprenti à archiver
+    nom="SousFifre"
+    prenom="Malheureux"
+    photo="/url/photo.jpg"
+    login=generate_login(nom, prenom)
+    
+    add_apprenti(nom, prenom, login, photo, commit=False)
+    id_apprenti = get_id_apprenti_by_login(login)
+    
+    # Connexion en tant que superadmin
+    superadmin = "JED10"
+    mdp = "superadmin"
+    connexion_personnel_mdp(client, superadmin, mdp)
+
+    # Test d'archivage d'un apprenti
+    response = client.get(url_for("api.api_archiver_apprenti", id_apprenti=id_apprenti))
+
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == "/api/archiver-apprenti/"+str(id_apprenti)
+    
+    # Test de vérification de l'archivage
+    assert response.json["valide"] == True
+    
+    # Test de désarchivage
+    response = client.get(url_for("api.api_desarchiver_apprenti", id_apprenti=id_apprenti))
+    
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == "/api/desarchiver-apprenti/"+str(id_apprenti)
+    
+    # Test de vérification du désarchivage
+    assert response.json["valide"] == True
+    
+    # Test de suppression
+    response = client.get(url_for("api.api_supprimer_apprenti", id_apprenti=id_apprenti))
+    
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == "/api/supprimer-apprenti/"+str(id_apprenti)
+    
+    # Test de vérification de la suppression
+    assert response.json["valide"] == True
+    
+# Test de l'archivage d'un personnel
+def test_archivage_personnel(client):
+    # Partie archivage
+    # Récupération d'un personnel
+    login = "ALL11"
+    id_personnel = get_id_personnel_by_login(login)
+    
+    # Connexion en tant que superadmin
+    superadmin = "JED10"
+    mdp = "superadmin"
+    connexion_personnel_mdp(client, superadmin, mdp)
+
+    # Test d'archivage d'un personnel
+    response = client.get(url_for("api.api_archiver_personnel", id_personnel=id_personnel))
+
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == "/api/archiver-personnel/"+str(id_personnel)
+    
+    # Test de vérification de l'archivage
+    assert response.json["valide"] == True
+    
+    # Test de désarchivage
+    response = client.get(url_for("api.api_desarchiver_personnel", id_personnel=id_personnel))
+    
+    # Test d'accès à la route
+    assert response.status_code == 200
+    
+    # Test de vérification de la route
+    assert response.request.path == "/api/desarchiver-personnel/"+str(id_personnel)
+    
+    # Test de vérification du désarchivage
+    assert response.json["valide"] == True
