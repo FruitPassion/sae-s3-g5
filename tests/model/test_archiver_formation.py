e706cd138fa282773b400536d489c9b506f10905
Mely2612 <iyamu34@gmail.com>
2023-12-18 09:06:36 +0100

    modif test_archiver_formation

--- tests/model/test_archiver_formation.py
+++ tests/model/test_archiver_formation.py
@@ -1,14 +1,12 @@
-from flask import url_for
 from model_db.formation import Formation
 from model_db.shared_model import db
 
 def test_archiver_formation(client):
-    # Récupérez l'ID de la formation existante
-    id_formation = formation.id
-
+    
+    id_formation = 2
     # Récupérer la formation existante
     formation = Formation.query.filter_by(id_formation=id_formation).first()
-
+   
 
     if not formation:
         raise ValueError(f"Aucune formation trouvée avec l'id {id_formation}")
@@ -20,7 +18,7 @@ def test_archiver_formation(client):
     db.session.commit()
 
     # Vérifier que la formation a bien été archivée de la base de données
-    assert db.session.query(Formation).get(id_formation) is None
+    assert db.session.query(Formation).get(id_formation).archive == True
 
     # Effectuer d'autres vérifications si nécessaire   
     db.session.rollback()
