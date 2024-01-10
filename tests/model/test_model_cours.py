88892612c27c81ac2c72e3161842cd4368b35e4d
Mely2612 <iyamu34@gmail.com>
2024-01-10 09:15:41 +0100

    test modifier cours terminé

--- tests/model/test_model_cours.py
+++ tests/model/test_model_cours.py
@@ -1,4 +1,4 @@
-from custom_paquets.tester_usages import CoursTest
+from custom_paquets.tester_usages import CoursTest, CoursTestModif
 from model.shared_model import Cours, db
 
 def test_ajouter_cours(client):
@@ -8,3 +8,18 @@ def test_ajouter_cours(client):
     assert db.session.query(Cours).filter(Cours.id_cours == cours_t.id_cours).first() is not None
     db.session.rollback()
     assert db.session.query(Cours).filter(Cours.id_cours == cours_t.id_cours).first() is None
+
+def test_modifier_cours(client):
+    cours_t = CoursTest()
+
+    cours_m = CoursTestModif(cours_t.id_cours)
+    
+    # Vérification que le cours soit bien modifé
+    cours_modif: Cours = db.session.query(Cours).filter(Cours.id_cours == cours_m.id_cours).first()
+    assert cours_modif is not None
+    assert cours_modif.theme == cours_m.theme
+    assert cours_modif.cours == cours_m.cours
+    assert cours_modif.duree == cours_m.duree
+    assert cours_modif.id_formation == cours_m.id_formation
+
+    db.session.rollback()
