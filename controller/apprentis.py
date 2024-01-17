4c4b8687973483be15fbb6038eed253d8163658e
Mirindra RANDRIANARISON <mirindra.randrianarison-ratsiandavana@etu.iut-tlse3.fr>
2024-01-17 10:48:48 +0100

    ajout composition fiche apprenti export pdf

--- controller/apprentis.py
+++ controller/apprentis.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template, session, request
 from custom_paquets.builder import build_categories
 from custom_paquets.converter import changer_date
 from custom_paquets.decorateur import apprenti_login_required
-from model.apprenti import get_apprenti_by_login
-from model.composer import get_composer_presentation, maj_contenu_fiche
+from model.apprenti import get_apprenti_by_login, get_id_apprenti_by_login
+from model.composer import get_composer_presentation, get_composer_presentation_par_apprenti, maj_contenu_fiche
 from model.ficheintervention import get_etat_fiche_par_id_fiche, get_fiches_techniques_par_login, get_nom_cours_by_id, \
    get_id_fiche_apprenti, get_fiche_par_id_fiche
 
@@ -95,5 +95,5 @@ def imprimer_pdf(numero):
     """
     # En attente de la complétion de la fiche
     fiche = get_fiche_par_id_fiche(get_id_fiche_apprenti(session['name'], numero))
-    composer_fiche = get_composer_presentation(get_id_fiche_apprenti(session['name'], numero))
+    composer_fiche = get_composer_presentation_par_apprenti(get_id_fiche_apprenti(session['name'], numero))
     return render_template("apprentis/fiche_pdf.html", composition=composer_fiche, fiche=fiche)
