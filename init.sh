e54fa5e53aa82f7debf047d804f1755894adb421
FruitPassion <gamehotdl@gmail.com>
2024-02-12 19:43:14 +0100

    Mise a jour de la base de donnée

--- init.sh
+++ init.sh
@@ -9,17 +9,18 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'         
 WHITE='\033[0;37m'
 NC='\033[0m'
+BALISE='##############################'
 
 # Update of the packages
-printf "${PURPLE}Mise à jour des paquets ...${NC}\n\n"
+printf "$BALISE\n${PURPLE}Mise à jour des paquets ...${NC}\n$BALISE\n\n"
 apt update && apt upgrade # check
 
 # Install the dependencies
-printf "\n\n${PURPLE}Installation des dépendances ...${NC}\n\n"
+printf "\n\n$BALISE\n${PURPLE}Installation des dépendances ...${NC}\n$BALISE\n\n"
 apt install -y git apache2 libapache2-mod-wsgi-py3 mariadb-server wget build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev #check
 
 # Install python
-printf "\n\n${PURPLE}Installation de ${GREEN}python${PURPLE} (cette étape peut prendre du temps) ...${NC}\n\n"
+printf "\n\n$BALISE\n${PURPLE}Installation de ${GREEN}python${PURPLE} (cette étape peut prendre du temps) ...${NC}\n$BALISE\n\n"
 
 wget -c https://www.python.org/ftp/python/3.10.13/Python-3.10.13.tar.xz # check
 tar -Jxvf Python-3.10.13.tar.xz # check
@@ -27,15 +28,17 @@ cd Python-3.10.13 #check
 ./configure --enable-optimizations --prefix=/usr/local --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib" #check
 sudo make -j4 && sudo make altinstall # check 
 cd .. # check
-rm Python-3.10.13.tar.xz Python-3.10.13 # check
+rm -r Python-3.10.13.tar.xz Python-3.10.13 # check
 
-printf "\n\n${GREEN}Installation des requirements python${NC}\n\n" # check
+printf "\n\n$BALISE\n${GREEN}Installation des requirements python${NC}\n$BALISE\n\n" # check
 
 pip3.10 install -r requirements.txt # check
 
+pip3.10 install --upgrade pip # check
+
 # Initialization
 #### MARIADB ####
-printf "\n\n${YELLOW}Paramétrage de la base de donnees MariaDB${NC}\n\n"
+printf "\n\n$BALISE\n${YELLOW}Paramétrage de la base de donnees MariaDB${NC}\n$BALISE\n\n"
 
 read -p "Entrez un nom d'administrateur : " 
 echo    # (optional) move to a new line
@@ -53,38 +56,38 @@ repmdp=$REPLY
 
 repmdp=$(python3.10 -c "from custom_paquets.security import encrypt_password; print(encrypt_password('$repmdp').decode('utf-8'))")
 
-sed -i -e "s/--AREMPLACERNOM--/$repnom/" db_production.py
-sed -i -e "s/--AREMPLACERPRENOM--/$repprenom/" db_production.py
-sed -i -e "s/--AREMPLACERLOG--/$replog/" db_production.py
-sed -i -e "s/--AREMPLACERMDP--/$repmail/" db_production.py
-sed -i -e "s/--AREMPLACERMAIL--/$repmdp/" db_production.py
+sed -i -e "s/--AREMPLACERNOM--/$repnom/" db_production.sql
+sed -i -e "s/--AREMPLACERPRENOM--/$repprenom/" db_production.sql
+sed -i -e "s/--AREMPLACERLOG--/$replog/" db_production.sql
+sed -i -e "s/--AREMPLACERMDP--/$repmail/" db_production.sql
+sed -i -e "s/--AREMPLACERMAIL--/$repmdp/" db_production.sql
 
-printf "\n\n${YELLOW}Démmarage de MariaDB${NC}\n\n"
+printf "\n\n$BALISE\n${YELLOW}Démmarage de MariaDB${NC}\n$BALISE\n\n"
 
 sudo systemctl enable mariadb.service # check
 sudo systemctl start mariadb.service # check
 
 
-printf "\n\n${RED}Génération du mot de passe utilisateur${NC}\n\n" # check
+printf "\n\n$BALISE\n${RED}Génération du mot de passe utilisateur${NC}\n$BALISE\n" # check
 pwdadm=$(date | sha256sum) # check
 pwdadm=$(echo "${pwdadm// -}") # check
 pwdadm=$(echo "${pwdadm// }") # check
 
 echo "root :" "'$pwdadm'" > db_adm_psswd.txt
-printf "\n\n${RED}Stockage du mot de passe root dans le fichier db_adm_psswd.txt${NC}\n\n"
+printf "$BALISE\n${RED}>>> Stockage du mot de passe root dans le fichier db_adm_psswd.txt${NC}\n$BALISE\n\n"
 
 
-printf "\n\n${RED}Génération du mot de passe utilisateur${NC}\n\n" # check
+printf "\n\n$BALISE\n${RED}Génération du mot de passe utilisateur${NC}\n$BALISE\n" # check
 pwdusr=$(date | sha256sum) # check
 pwdusr=$(echo "${pwdusr// -}") # check
 pwdusr=$(echo "${pwdusr// }") # check
 
 echo "user :" "'$pwdusr'" > db_usr_psswd.txt # check
-printf "\n\n${RED}Stockage du mot de passe root dans le fichier db_usr_psswd.txt${NC}\n\n"
+printf "$BALISE\n${RED}>>> Stockage du mot de passe root dans le fichier db_usr_psswd.txt${NC}\n$BALISE\n\n"
 
 sed -i -e "s/--AREMPLACER--/$pwdusr/" config.py
 
-printf "\n\n${YELLOW}Initialisation de la base de donnees MariaDB${NC}\n\n"
+printf "\n\n$BALISE\n${YELLOW}Initialisation de la base de donnees MariaDB${NC}\n$BALISE\n\n"
 
 ## Create USER
 mysql -e "CREATE OR REPLACE USER 'user'@'localhost' IDENTIFIED BY '$pwdusr';" # check
@@ -96,10 +99,6 @@ mysql -e "flush privileges;" # check
 mysql -h "localhost" -u "user" "-p$pwdusr" "db_fiches_prod" < "db_production.sql"
 
 # Secure mariadb installation
-# Kill the anonymous users
-mysql -e "DROP USER ''@'localhost'" # check
-# Because our hostname varies we'll use some Bash magic here.
-mysql -e "DROP USER ''@'$(hostname)'" # check
 # Kill off the demo database
 mysql -e "DROP DATABASE IF EXISTS test" # check
 # Make our changes take effect
@@ -107,7 +106,7 @@ mysql -e "FLUSH PRIVILEGES" # check
 # Make sure that NOBODY can access the server without a password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$pwdadm'; FLUSH PRIVILEGES;" # check
 
-printf "\n\n${YELLOW}Initialisation et paramétrage de la base de donnees terminée${NC}\n\n"
+printf "\n\n$BALISE\n${YELLOW}Initialisation et paramétrage de la base de donnees terminée${NC}\n$BALISE\n\n"
 
 # #### APACHE #### 
 echo "Initialization of Apache..."
