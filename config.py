5391413a69b66e5fac30dbde72e4ce875627fdb9
FruitPassion <gamehotdl@gmail.com>
2024-02-13 17:59:15 +0100

    Ajout chiffrement config + correction init

--- config.py
+++ config.py
@@ -1,9 +1,30 @@
 import os
 import logging
+import sys
+from configparser_crypt import ConfigParserCrypt
+
+from custom_paquets.app_checker import lire_config
 
 import pymysql
 
 pymysql.install_as_MySQLdb()
+
+config = lire_config("config.txt")
+
+if config == "prod":
+    file = 'dbs.encrypted'
+    conf_file = ConfigParserCrypt()
+    try:
+        with open('key.encrypt', 'rb') as passwd:
+            password = passwd.read()
+    except Exception as error:
+        print("Fichier clée manquant")
+        sys.exit(1)
+    conf_file.aes_key = password
+    conf_file.read_encrypted(file)
+else:
+    conf_file = {'DBS': {'db_password': 'password'}}
+
 
 
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -42,7 +63,7 @@ class DevConfig:
     SESSION_PERMANENT = False
     WTF_CSRF_ENABLED = False
     SESSION_TYPE = "filesystem"
-    DB_SCHEMA = "db_fiches_dev"
+    DB_SCHEMA = f"db_fiches_{config.lower()}"
     SQLALCHEMY_DATABASE_URI = f'mariadb://local_user:password@localhost:3306/{DB_SCHEMA}'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     REMEMBER_COOKIE_SAMESITE = "strict"
@@ -62,8 +83,9 @@ class ProdConfig:
     DEBUG = False
     SESSION_PERMANENT = False
     SESSION_TYPE = "filesystem"
-    DB_SCHEMA = "db_fiches_prod"
-    SQLALCHEMY_DATABASE_URI = f'mariadb://user:--AREMPLACER--@localhost:3306/{DB_SCHEMA}'
+    DB_SCHEMA = f"db_fiches_{config.lower()}"
+    SQLALCHEMY_DATABASE_URI = 'mariadb://user:{}@localhost:3306/{}'.format(
+        conf_file['DBS']['db_password'], DB_SCHEMA)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     REMEMBER_COOKIE_SAMESITE = "strict"
     SQLALCHEMY_ENGINE_OPTIONS = {
