b1266e7d868abdbcf40ec103aff7e85501734207
Mely2612 <iyamu34@gmail.com>
2024-03-06 21:12:06 +0100

    barre de recherche educ admin (en cours)

---
+++ static/js/recherche_cours.js
@@ -0,0 +1,32 @@
+function searchTable(tabname, searchCours)
+{
+    var noresultname = tabname + 'Res';
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById(searchCours);
+    filter = input.value.toUpperCase();
+    table = document.getElementById(tabname);
+    tr = table.getElementsByClassName("row-to-search");
+    var nbr = 0;
+    for (i = 0; i < tr.length; i++) {
+        var display = "none";
+        for (var j = 0; j < tr[i].getElementsByTagName("td").length; j++) {
+            td = tr[i].getElementsByTagName("td")[j];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    display = "";
+                    nbr++;
+                } 
+            }
+        }
+        tr[i].style.display = display;
+    }
+
+    if (nbr == 0) {
+        document.getElementById(noresultname).style.display = "";
+        table.style.display = "none";
+    } else {
+        document.getElementById(noresultname).style.display = "none";
+        table.style.display = "";
+    }
+}
