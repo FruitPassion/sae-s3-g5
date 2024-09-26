d6af275c8abe6506bc8266db200dc5830ddc1e08
raphael <gamehotdl@gmail.com>
2024-09-26 16:53:54 +0200

    Refactor: Add .flake8 configuration file and cleancode.sh script for code formatting

---
+++ cleancode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR="."
+css_files=$(find . -type f -name "*.css" -not -path "*/.venv/*")
+js_files=$(find . -type f -name "*.js" -not -path "*/.venv/*")
+html_files=$(find . -type f -name "*.html" -not -path "*/.venv/*")
+
+: "${VIRTUAL_ENV?Python virtual env must be active}"
+
+echo "Executing flake8..." && flake8 $BASE_DIR --exclude ".venv/*"
+echo "Executing DjangoLinter..." && djlint $html_files --lint
+echo "Executing isort..." && isort $BASE_DIR
+echo "Executing black..." && black $BASE_DIR
+echo "Executing JS-Beautifier..." && js-beautify $js_files
+echo "Executing CSS-Beautifier..." && css-beautify -r $css_files
+echo "Executing DJFormater..." && djlint $html_files --quiet --reformat
