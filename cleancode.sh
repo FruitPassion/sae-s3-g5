e5b28e4b4d94ee72c184de1371aefd040ad9f00e
Boudin Raphael <gamehotdl@gmail.com>
2024-11-19 18:24:15 +0100

    chore: Modify flake8 parameter

--- cleancode.sh
+++ cleancode.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-BASE_DIR="."
-css_files=$(find . -type f -name "*.css" -not -path ".venv/*")
-js_files=$(find . -type f -name "*.js" -not -path ".venv/*")
-html_files=$(find . -type f -name "*.html" -not -path ".venv/*")
+BASE_DIR="app"
+css_files=$(find $BASE_DIR -type f -name "*.css")
+js_files=$(find $BASE_DIR -type f -name "*.js")
+html_files=$(find $BASE_DIR -type f -name "*.html")
 
 : "${VIRTUAL_ENV?Python virtual env must be active}"
 
 echo "Executing black..." && black $BASE_DIR
 echo "Executing isort..." && isort $BASE_DIR
-echo "Executing flake8..." && flake8 $BASE_DIR --exclude ".venv/*","tests/*"
+echo "Executing flake8..." && flake8 $BASE_DIR --exclude "app/tests/*","app/migrations/*"
 echo "Executing DjangoLinter..." && djlint $html_files --lint
 echo "Executing JS-Beautifier..." && js-beautify $js_files
 echo "Executing CSS-Beautifier..." && css-beautify -r $css_files
